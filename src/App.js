2aa8e1713f4b3da1845541e50b7a18af49cbb5be
Derek <solerpower1@hotmail.com>
2019-06-19 16:20:20 -0700

    Checkpoint-4-SetUsername: Completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import * as firebase from 'firebase';
 import RoomList from './components/RoomList';
 import MessageList from './components/MessageList';
+import User from './components/User';
 
 
 var firebaseConfig = {
@@ -19,14 +20,19 @@ firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       activeRoom: null,
+      user: null
     };
   }
 
   setActiveRoom(room) {
     this.setState({ activeRoom: room });
+  }
+
+  setUser(user) {
+    this.setState({ user: user });
   }
 
   render() {
@@ -35,6 +41,9 @@ class App extends Component {
         <aside id="sidebar">
           <h1 className="App-title">Bloc Chat</h1>
           <RoomList firebase={firebase} activeRoom={this.state.activeRoom} setActiveRoom={this.setActiveRoom.bind(this)} />
+        </aside>
+        <aside id="sidebar-bottom">
+          <User firebase={firebase} user={this.state.user} setUser={this.setUser.bind(this)} />
         </aside>
         <MessageList firebase={firebase} activeRoom={this.state.activeRoom} />
       </div>
